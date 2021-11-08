b61eb69ff0524ce4bb96bd8c8182251fc1a34b09
steven <stevenmoreno498@gmail.com>
2021-11-07 19:43:18 -0500

    Subir archvio

---
+++ Constructores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Constructores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Window W = new Window();
+            W.show();
+
+        }
+    }
+
+    class Window
+    {
+        private int width, high;
+        private string colour;
+        public Window()
+        {
+            width = 20;
+            high = 25;
+            colour = "Red";
+        }
+
+        public void show()
+        {
+            Console.WriteLine("THE DIMENSION OF THE SCREEN IS: " + width + "x" + high + " THE COLOR IS: " + colour);
+        }
+        /*~Window() Destroyer
+        {
+
+        }
+        */
+    }
+       
+}
